3f9db7d65967e07824817f989c85fa648dd1e629
777Samael <kepka.przemyslaw@gmail.com>
2022-06-20 10:08:52 +0200

    nowe dane

---
+++ week1/code.py
@@ -0,0 +1,12 @@
+import numpy as np      # linear algebra
+import pandas as pd     # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+import warnings
+warnings.filterwarnings("ignore")
+
+df = pd.read_csv("../input/heart.csv")
+df.info()
+
