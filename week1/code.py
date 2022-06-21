c52408f4c6a0233633215cddf1f3f8169e02d20d
777Samael <kepka.przemyslaw@gmail.com>
2022-06-21 09:04:05 +0200

    próba zrobienia w pc

--- week1/code.py
+++ week1/code.py
@@ -2,7 +2,7 @@ import numpy as np      # linear algebra
 import pandas as pd     # data processing, CSV file I/O (e.g. pd.read_csv)
 import seaborn as sns
 import matplotlib.pyplot as plt
-# %matplotlib inline
+%matplotlib inline
 
 import warnings
 warnings.filterwarnings("ignore")
